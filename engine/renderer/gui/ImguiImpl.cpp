b282a90fd20b6f729978c4c3354719604be03447
Max Schafer <maxsg5@hotmail.com>
2024-06-23 23:46:15 -0600

    WIP experimenting with demo windows

--- engine/renderer/gui/ImguiImpl.cpp
+++ engine/renderer/gui/ImguiImpl.cpp
@@ -1,5 +1,6 @@
 #include "ImguiImpl.hpp"
 
+#include "../../../editor/utils/Utils.hpp"
 #include "GLFW/glfw3.h"
 
 namespace Quirk::Engine::Renderer::Gui
@@ -40,6 +41,34 @@ namespace Quirk::Engine::Renderer::Gui
         ImGui::NewFrame();
         ImGui::ShowDemoWindow();
         ImGui::DockSpaceOverViewport();
+        static Editor::Uitls::ExampleAppConsole console;
+        console.Draw("Console",nullptr);
+
+        static Editor::Uitls::ExampleAppLog log;
+
+        // For the demo: add a debug button _BEFORE_ the normal log window contents
+        // We take advantage of a rarely used feature: multiple calls to Begin()/End() are appending to the _same_ window.
+        // Most of the contents of the window will be added by the log.Draw() call.
+        ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Example: Log");
+        if (ImGui::SmallButton("[Debug] Add 5 entries"))
+        {
+            static int counter = 0;
+            const char* categories[3] = { "info", "warn", "error" };
+            const char* words[] = { "Bumfuzzled", "Cattywampus", "Snickersnee", "Abibliophobia", "Absquatulate", "Nincompoop", "Pauciloquent" };
+            for (int n = 0; n < 5; n++)
+            {
+                const char* category = categories[counter % IM_ARRAYSIZE(categories)];
+                const char* word = words[counter % IM_ARRAYSIZE(words)];
+                log.AddLog("[%05d] [%s] Hello, current time is %.1f, here's a word: '%s'\n",
+                    ImGui::GetFrameCount(), category, ImGui::GetTime(), word);
+                counter++;
+            }
+        }
+        ImGui::End();
+
+        // Actually call in the regular Log helper (which will Begin() into the same window as we just did)
+        log.Draw("Example: Log", nullptr);
         
     }
 
